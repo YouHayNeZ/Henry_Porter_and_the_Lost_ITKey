ed1f104fc35ebc27fedafb87048a0a8010af8a7d
johannesvorbach <Johannes.Vorbach@tum.de>
2024-01-08 08:53:11 +0100

    Did the wall class (extends Entity), tried to take care of where wall is already and if it is bounded so that the inner and outer border look different (hope it works later on)

--- core/src/de/tum/cit/ase/maze/entity/Wall.java
+++ core/src/de/tum/cit/ase/maze/entity/Wall.java
@@ -1,3 +1,41 @@
 package de.tum.cit.ase.maze.entity;
 
-//public class Wall extends Entity {}
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
+
+import de.tum.cit.ase.maze.MazeRunnerGame;
+
+public class Wall extends Entity {
+
+    public enum RepresentationType {
+
+        LOWER_WITHOUT_UPPER(0),
+        CENTER_WITH_UPPER_AND_LOWER(1),
+        LOWER_WITH_UPPER(2),
+        UPPER(3);
+
+        int imageIndex;
+
+        RepresentationType(int imageIndex) {
+            this.imageIndex = imageIndex;
+        }
+    }
+
+    Array<TextureRegion> textureRegionArray;
+    RepresentationType representationType;
+
+    public Wall(MazeRunnerGame game) {
+        super(game);
+        textureRegionArray = game.getWallTextureRegionArray();
+        setRepresentationType(RepresentationType.CENTER_WITH_UPPER_AND_LOWER);
+    }
+
+    public void setRepresentationType(RepresentationType type) {
+        this.representationType = type;
+        setTextureRegion(textureRegionArray.get(representationType.imageIndex));
+    }
+
+    public RepresentationType getRepresentationType() {
+        return representationType;
+    }
+}
